591d1c95b1b435aac913f2f8d3f9d6e0e128ad39
szymon-rogus <sszymek98@gmail.com>
2024-10-19 18:00:54 +0200

    Slice in store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./slices/filterSlice";
+import alertReducer from "./slices/alertConfigurationSlice";
 
 export default configureStore({
   reducer: {
     filter: filterReducer,
+    alertReducer,
   },
 });
